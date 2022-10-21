e21dcd4078e0025aa8482ead914d7ab7bd3d2ad1
jjuod <j.juodakis@gmail.com>
2022-10-21 17:22:37 +0200

    consensus adjusted ref mapping

--- redo-old.sh
+++ redo-old.sh
@@ -76,37 +76,156 @@ cd ~/Documents/mieziai/old/called/
 #	awk 'NR==1{print $1,$2,$3,$4,$5,$6,"DP4","MAF",$8; next} {split($7,d,","); rr=d[1]+d[2]; aa=d[3]+d[4]; print $1, $2, $3, $4, $5, $6, rr+aa, aa/(rr+aa), $8}' calls_tw-12_HVgp_chr${i}.txt > callsf_tw-12_HVgp_chr${i}.txt
 #done
 
-
-# TESTING
-# generate consensus:
-# normalize vcf
+# 8. GENERATING AII-11 CONSENSUS
 cd ~/Documents/mieziai/old/called/
-/mnt/hdd/soft/bcftools-1.16/bcftools norm /mnt/quick/julius-temp/called_AII-11_chr4.vcf.gz \
-       	-f /mnt/quick/julius-temp/Hordeum_vulgare_goldenpromise.GPv1.dna.toplevel.fa \
-	-Oz -o normd_AII-11_chr4.vcf.gz
-bcftools index normd_AII-11_chr4.vcf.gz
+echo "generating consensus from old samples" 
+for CHR in 2 3 7
+do
+	# normalize vcf to clean up indels
+	/mnt/hdd/soft/bcftools-1.16/bcftools norm /mnt/quick/julius-temp/called_AII-11_chr${CHR}.vcf.gz \
+	       	-f /mnt/quick/julius-temp/Hordeum_vulgare_goldenpromise.GPv1.dna.toplevel.fa \
+		-Oz -o normd_AII-11_chr${CHR}.vcf.gz
+	bcftools index normd_AII-11_chr${CHR}.vcf.gz
+	
+	# extract each chr references
+	awk '$0~/^>contig'${CHR}'/{p=1;print;next} p==1{if($0~/^>/){exit}; print}' /mnt/quick/julius-temp/Hordeum_vulgare_goldenpromise.GPv1.dna.toplevel.fa > ../../refs/Hordeum_vulgare_goldenpromise.dna.chr${CHR}.fa
+	bwa index -a bwtsw ../../refs/Hordeum_vulgare_goldenpromise.dna.chr${CHR}.fa
+	
+	# create consensus, taking alternate allele in all variable positions
+	/mnt/hdd/soft/bcftools-1.16/bcftools consensus -HA \
+		-f ../../refs/Hordeum_vulgare_goldenpromise.dna.chr${CHR}.fa normd_AII-11_chr${CHR}.vcf.gz -i'DP>5' > consensus_AII-11_chr${CHR}.fa 2> cons_AII-11_chr${CHR}.log
+	# Around 2k/1.1M per chr are skipped, mostly indels overlapping other variants.
+	rm ../../refs/Hordeum_vulgare_goldenpromise.dna.chr${CHR}.fa 
+	rm ../../refs/Hordeum_vulgare_goldenpromise.dna.chr${CHR}.fa.sa
+	rm ../../refs/Hordeum_vulgare_goldenpromise.dna.chr${CHR}.fa.pac 
+	rm ../../refs/Hordeum_vulgare_goldenpromise.dna.chr${CHR}.fa.amb
+	rm ../../refs/Hordeum_vulgare_goldenpromise.dna.chr${CHR}.fa.ann 
+	rm ../../refs/Hordeum_vulgare_goldenpromise.dna.chr${CHR}.fa.bwt 
+	bwa index -a bwtsw consensus_AII-11_chr${CHR}.fa
+done
 
-# extract 1chr reference
-grep -n contig /mnt/quick/julius-temp/Hordeum_vulgare_goldenpromise.GPv1.dna.toplevel.fa
-awk 'NR>=19795980 && NR<29362982' /mnt/quick/julius-temp/Hordeum_vulgare_goldenpromise.GPv1.dna.toplevel.fa > ../../refs/Hordeum_vulgare_goldenpromise.dna.chr4.fa
-bwa index -a bwtsw ../../refs/Hordeum_vulgare_goldenpromise.dna.chr4.fa
-
-# create consensus
-/mnt/hdd/soft/bcftools-1.16/bcftools consensus -HA -f ../../refs/Hordeum_vulgare_goldenpromise.dna.chr4.fa normd_AII-11_chr4.vcf.gz -i'DP>5' > consensus_AII-11_chr4.fa 2> cons_AII-11_chr4.log
-# Around 2k/1.1M are skipped, mostly indels overlapping other variants
+# 9. MAP NEW POOLS FROM THE CONSENSUS
 
 cd ~/Documents/mieziai/2022/mapped/
+echo "mapping the separate pools"
 
-# extract mapped fastqs
-samtools view -@4 -f PROPER_PAIR -b AII_HVgp_nodups.bam contig4 \
-	-o /mnt/quick/julius-temp/mapping/AII_HVgp_nodups_chr4.bam
-samtools collate -@4 -f -u -O /mnt/quick/julius-temp/mapping/AII_HVgp_nodups_chr4.bam |\
-       	samtools fastq -@4 -1 AII_HVgp_nodups_chr4_1.fastq -2 AII_HVgp_nodups_chr4_2.fastq
-gzip AII_HVgp_nodups_chr4_1.fastq
-gzip AII_HVgp_nodups_chr4_2.fastq
+for CHR in 1 2 3 7
+do
+	# extract (properly) mapped fastqs
+	samtools view -@4 -f PROPER_PAIR -b AII_HVgp_nodups.bam contig${CHR} \
+	        -o /mnt/quick/julius-temp/mapping/AII_HVgp_nodups_chr${CHR}.bam
+	samtools collate -@4 -f -u -O /mnt/quick/julius-temp/mapping/AII_HVgp_nodups_chr${CHR}.bam |\
+	       	samtools fastq -@4 -1 AII_HVgp_nodups_chr${CHR}_1.fastq \
+		-2 AII_HVgp_nodups_chr${CHR}_2.fastq
+	gzip AII_HVgp_nodups_chr${CHR}_1.fastq
+	gzip AII_HVgp_nodups_chr${CHR}_2.fastq
+	samtools view -@4 -f PROPER_PAIR -b tw_HVgp_nodups.bam contig${CHR} \
+		-o /mnt/quick/julius-temp/mapping/tw_HVgp_nodups_chr${CHR}.bam
+	samtools collate -@4 -f -u -O /mnt/quick/julius-temp/mapping/tw_HVgp_nodups_chr${CHR}.bam |\
+	       	samtools fastq -@4 -1 tw_HVgp_nodups_chr${CHR}_1.fastq \
+		-2 tw_HVgp_nodups_chr${CHR}_2.fastq
+	gzip tw_HVgp_nodups_chr${CHR}_1.fastq
+	gzip tw_HVgp_nodups_chr${CHR}_2.fastq
 
-bwa mem -t 9 ~/Documents/mieziai/old/called/consensus_AII-11_chr4.fa \
-	AII_HVgp_nodups_chr4_1.fastq.gz AII_HVgp_nodups_chr4_2.fastq.gz \
-	2> bwa_cons_chr4_log.txt | samtools fixmate -@2 -m - - | \
-	samtools sort -@4 -m 3500M -o /mnt/quick/julius-temp/mapping/AII_HVgp_cons_chr4.bam
+	rm /mnt/quick/julius-temp/mapping/AII_HVgp_nodups_chr${CHR}.bam
+	rm /mnt/quick/julius-temp/mapping/tw_HVgp_nodups_chr${CHR}.bam
+	
+	# re-map them
+	bwa mem -t 10 ~/Documents/mieziai/old/called/consensus_AII-11_chr${CHR}.fa \
+		AII_HVgp_nodups_chr${CHR}_1.fastq.gz AII_HVgp_nodups_chr${CHR}_2.fastq.gz \
+		2> bwa_cons_chr${CHR}_log.txt | samtools fixmate -@2 -m - - | \
+		samtools sort -@4 -m 3500M -o /mnt/quick/julius-temp/mapping/AII_HVgp_cons_chr${CHR}.bam
+	bwa mem -t 10 ~/Documents/mieziai/old/called/consensus_AII-11_chr${CHR}.fa \
+		tw_HVgp_nodups_chr${CHR}_1.fastq.gz tw_HVgp_nodups_chr${CHR}_2.fastq.gz \
+		2> bwa_cons_tw_chr${CHR}_log.txt | samtools fixmate -@2 -m - - | \
+		samtools sort -@4 -m 3500M -o /mnt/quick/julius-temp/mapping/tw_HVgp_cons_chr${CHR}.bam
+done
 
+# 10. CALL FROM THE RE-MAPS ON CONSENSUS
+cd ~/Documents/mieziai/2022/called/
+echo "calling on consensus"
+
+for CHR in 1 2 3 7
+do
+	bcftools mpileup \
+	    -Ou -f ~/Documents/mieziai/old/called/consensus_AII-11_chr${CHR}.fa \
+	    /mnt/quick/julius-temp/mapping/AII_HVgp_cons_chr${CHR}.bam |\
+	    bcftools call -m -v -Oz -o tmp_calledcons_AII_chr${CHR}.vcf.gz
+	bcftools mpileup \
+	    -Ou -f ~/Documents/mieziai/old/called/consensus_AII-11_chr${CHR}.fa \
+	    /mnt/quick/julius-temp/mapping/tw_HVgp_cons_chr${CHR}.bam |\
+	    bcftools call -m -v -Oz -o tmp_calledcons_tw_chr${CHR}.vcf.gz
+	
+	# parse calls, calculate MAF
+	echo 'POS REF ALT DP MQ MQ0F DP4 MAF GT' > callsconsf_AII_HVgp_chr${CHR}.txt
+	bcftools query -f '%POS %REF %ALT %DP %MQ %MQ0F %DP4 [%GT ]\n' \
+		tmp_calledcons_AII_chr${CHR}.vcf.gz | \
+		awk '{split($7,d,","); rr=d[1]+d[2]; aa=d[3]+d[4]; print $1, $2, $3, $4, $5, $6, rr+aa, aa/(rr+aa), $8}' >> callsconsf_AII_HVgp_chr${CHR}.txt
+	echo 'POS REF ALT DP MQ MQ0F DP4 MAF GT' > callsconsf_tw_HVgp_chr${CHR}.txt
+	bcftools query -f '%POS %REF %ALT %DP %MQ %MQ0F %DP4 [%GT ]\n' \
+		tmp_calledcons_tw_chr${CHR}.vcf.gz | \
+		awk '{split($7,d,","); rr=d[1]+d[2]; aa=d[3]+d[4]; print $1, $2, $3, $4, $5, $6, rr+aa, aa/(rr+aa), $8}' >> callsconsf_tw_HVgp_chr${CHR}.txt
+	
+done
+
+
+## TESTING
+
+# ALTERNATIVE CONSENSUS (only from homozygous gts)
+cd ~/Documents/mieziai/2022/mapped/
+#bwa mem -t 7 ~/Documents/mieziai/old/called/consensus_AII-11_chr${CHR}_hom.fa \
+#	AII_HVgp_nodups_chr${CHR}_1.fastq.gz AII_HVgp_nodups_chr${CHR}_2.fastq.gz \
+#	2> bwa_conshom_chr${CHR}_log.txt | samtools fixmate -@2 -m - - | \
+#	samtools sort -@4 -m 3500M -o /mnt/quick/julius-temp/mapping/AII_HVgp_conshom_chr${CHR}.bam
+#bwa mem -t 7 ~/Documents/mieziai/old/called/consensus_AII-11_chr${CHR}_hom.fa \
+#	tw_HVgp_nodups_chr${CHR}_1.fastq.gz tw_HVgp_nodups_chr${CHR}_2.fastq.gz \
+#	2> bwa_conshom_tw_chr${CHR}_log.txt | samtools fixmate -@2 -m - - | \
+#	samtools sort -@4 -m 3500M -o /mnt/quick/julius-temp/mapping/tw_HVgp_conshom_chr${CHR}.bam
+##done
+
+# TESTING
+
+# call from the re-maps on consensus
+cd ~/Documents/mieziai/2022/called/
+#for CHR in {4..6}
+#do
+CHR=5
+#bcftools mpileup \
+#    -Ou -f ~/Documents/mieziai/old/called/consensus_AII-11_chr${CHR}_hom.fa \
+#    /mnt/quick/julius-temp/mapping/AII_HVgp_conshom_chr${CHR}.bam |\
+#    bcftools call -m -v -Oz -o tmp_calledconshom_AII_chr${CHR}.vcf.gz
+#bcftools mpileup \
+#    -Ou -f ~/Documents/mieziai/old/called/consensus_AII-11_chr${CHR}_hom.fa \
+#    /mnt/quick/julius-temp/mapping/tw_HVgp_conshom_chr${CHR}.bam |\
+#    bcftools call -m -v -Oz -o tmp_calledconshom_tw_chr${CHR}.vcf.gz
+#
+## parse calls
+#echo 'POS REF ALT DP MQ MQ0F DP4 GT' > callsconshom_AII_HVgp_chr${CHR}.txt
+#bcftools query -f '%POS %REF %ALT %DP %MQ %MQ0F %DP4 [%GT ]\n' \
+#	tmp_calledconshom_AII_chr${CHR}.vcf.gz >> callsconshom_AII_HVgp_chr${CHR}.txt
+#awk 'NR==1{print $1,$2,$3,$4,$5,$6,"DP4","MAF",$8; next} {split($7,d,","); rr=d[1]+d[2]; aa=d[3]+d[4]; print $1, $2, $3, $4, $5, $6, rr+aa, aa/(rr+aa), $8}' callsconshom_AII_HVgp_chr${CHR}.txt > callsconshomf_AII_HVgp_chr${CHR}.txt
+#
+#echo 'POS REF ALT DP MQ MQ0F DP4 GT' > callsconshom_tw_HVgp_chr${CHR}.txt
+#bcftools query -f '%POS %REF %ALT %DP %MQ %MQ0F %DP4 [%GT ]\n' \
+#	tmp_calledconshom_tw_chr${CHR}.vcf.gz >> callsconshom_tw_HVgp_chr${CHR}.txt
+#awk 'NR==1{print $1,$2,$3,$4,$5,$6,"DP4","MAF",$8; next} {split($7,d,","); rr=d[1]+d[2]; aa=d[3]+d[4]; print $1, $2, $3, $4, $5, $6, rr+aa, aa/(rr+aa), $8}' callsconshom_tw_HVgp_chr${CHR}.txt > callsconshomf_tw_HVgp_chr${CHR}.txt
+
+# pooled consensus
+#bcftools mpileup \
+#    -Ou -f ~/Documents/mieziai/2022/called/consensus_AII_chr${CHR}.fa \
+#    /mnt/quick/julius-temp/mapping/tw_HVgp_conspool_chr${CHR}.bam |\
+#    bcftools call -m -v -Oz -o tmp_calledconspool_tw_chr${CHR}.vcf.gz
+#
+#echo 'POS REF ALT DP MQ MQ0F DP4 GT' > callsconspool_tw_HVgp_chr${CHR}.txt
+#bcftools query -f '%POS %REF %ALT %DP %MQ %MQ0F %DP4 [%GT ]\n' \
+#	tmp_calledconspool_tw_chr${CHR}.vcf.gz >> callsconspool_tw_HVgp_chr${CHR}.txt
+#awk 'NR==1{print $1,$2,$3,$4,$5,$6,"DP4","MAF",$8; next} {split($7,d,","); rr=d[1]+d[2]; aa=d[3]+d[4]; print $1, $2, $3, $4, $5, $6, rr+aa, aa/(rr+aa), $8}' callsconspool_tw_HVgp_chr${CHR}.txt > callsconspoolf_tw_HVgp_chr${CHR}.txt
+
+# MANTA SV calling:
+# (separate samples)
+cd ~/Documents/mieziai/old/svs/
+#MANTA=/mnt/hdd/soft/manta/manta-1.6.0.centos6_x86_64/bin
+#$MANTA/configManta.py \
+#	--referenceFasta /mnt/quick/julius-temp/Hordeum_vulgare_goldenpromise.GPv1.dna.toplevel.fa \
+#	--bam /mnt/quick/julius-temp/mapping/AII-11_HVgp.bam \
+#	--runDir ./
